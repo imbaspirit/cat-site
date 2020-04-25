66d7edddaa77caf2b766ad612c3e987d94770c29
imbaspirit <yamov.ra@gmail.com>
2020-04-25 22:13:05 +0300

    final update, last time zatupil

--- menu.php
+++ menu.php
@@ -2,5 +2,6 @@
 	<a href="index.php">Главная</a>
 	<a href="puzzles.php">Загадки</a>
 	<a href="guess.php">Разрешить спор</a>
-	<a href="gen.php">Генератор имен</a>
+	<a href="gen.php">Генератор кличек</a>
+	<a href="english.php">English</a>
 </div>
